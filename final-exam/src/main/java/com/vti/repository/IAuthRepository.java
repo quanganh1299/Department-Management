f5566f6c3db1fe93d9f5a7d41905d8bdfbe4c9dc
Quang Anh <140308195+quanganh1299@users.noreply.github.com>
2023-07-31 23:28:23 +0700

    Delete Backend

--- final-exam/src/main/java/com/vti/repository/IAuthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vti.repository;
-
-import com.vti.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAuthRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
