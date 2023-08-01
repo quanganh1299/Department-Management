50d18120d6756bd2832e06532797ec6162c0ed92
Quang Anh <140308195+quanganh1299@users.noreply.github.com>
2023-08-01 23:42:14 +0700

    Delete backend

--- final-exam/src/main/java/com/vti/service/IAuthService.java
+++
@@ -1,8 +0,0 @@
-package com.vti.service;
-
-import com.vti.form.AuthRegisterForm;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface IAuthService extends UserDetailsService {
-    void create(AuthRegisterForm form);
-}
